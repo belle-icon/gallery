9a217183c010c96831cc907f0b479b96d9ddb29a
awmleer <awmleer@icloud.com>
2022-01-23 19:15:07 +0800

    chore: adjust eslint rules

--- src/utils/decompress.ts
+++ src/utils/decompress.ts
@@ -6,17 +6,17 @@ function decompressGzText(buffer: ArrayBuffer) {
 }
 
 export function parseIcons(res: Response) {
-  return res.arrayBuffer()
+  return res
+    .arrayBuffer()
     .then(decompressGzText)
     .then(text => {
       // const dom = new DOMParser().parseFromString(string, "image/svg+xml")
       // Array.from(dom.getElementsByTagName('symbol'))
       return Array.from(
         text.matchAll(/<symbol id="(.*?)">(.*?)<\/symbol>/g)
-      )
-        .map(match => ({
-          name: match[1],
-          content: match[2].replace(/<(([a-z]+) [^\/>]+)\/>/g, '<$1></$2>')
-        }))
+      ).map(match => ({
+        name: match[1],
+        content: match[2].replace(/<(([a-z]+) [^/>]+)\/>/g, '<$1></$2>'),
+      }))
     })
 }
